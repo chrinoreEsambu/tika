332b2bacffdde5a60bdaf04fbd96bfe77a0fc322
chrinore <chrinoren@gmail.com>
2025-08-01 21:00:28 +0100

    started

--- backend/controllers/buyTicket.js
+++ backend/controllers/buyTicket.js
@@ -2,11 +2,11 @@ const argon2 = require("argon2");
 const prisma = require("../config/prismaClient");
 const session = require("express-session");
 
-
-expots.buyTicket = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+expots.buyTicket = async (req, res) => {
+  try {
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "somethings went wrong", error: error.message });
+  }
+};
