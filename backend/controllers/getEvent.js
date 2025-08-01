c9d3d80c0a79ebc94bf9c7298bca35c93e215b6c
chrinore <chrinoren@gmail.com>
2025-08-01 18:04:08 +0100

    started

--- backend/controllers/getEvent.js
+++ backend/controllers/getEvent.js
@@ -4,7 +4,13 @@ const session = require("express-session");
 
 exports.getEvents = async (req, res) => {
   try {
-    const findAllEvent = await prisma.event.findMany();
+    const findAllEvent = await prisma.event.findMany({
+      select: {
+        title: true,
+        date: true,
+        location: true,
+      },
+    });
     res.status(200).json({ message: "event available", findAllEvent });
   } catch (error) {}
 };
