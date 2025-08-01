9cf01b8ecdf68e98c700ef0e400f4478bce0e5cd
chrinore <chrinoren@gmail.com>
2025-08-01 18:10:11 +0100

    started

--- backend/controllers/getEvent.js
+++ backend/controllers/getEvent.js
@@ -1,11 +1,16 @@
 const argon2 = require("argon2");
 const prisma = require("../config/prismaClient");
-const session = require("express-session");
 
 exports.getEvents = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = 6;
+  const jumper = (page-1)*limit;
   try {
     const findAllEvent = await prisma.event.findMany({
+      skip: jumper,
+      take: limit,
       select: {
+        id_event: true,
         title: true,
         date: true,
         location: true,
