6961c4fed608bbcccd90a7685e5f72c646b956e6
chrinore <chrinoren@gmail.com>
2025-08-01 18:05:07 +0100

    started

--- backend/router/Allrouter.js
+++ backend/router/Allrouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const userControllers = require("../controllers/userControllers");
 const createEvents = require("../controllers/createEvents");
 const { validate } = require("../middleware/middeleware");
-const getEventsControllers =require("../controllers/getEvent")
-// const { event } = require("../controllers/createEvents");
+const getEventsControllers = require("../controllers/getEvent");
+
 const router = express.Router();
 
 router.post("/api/createUser", validate, userControllers.createUser);
 router.post("/api/createEvent", createEvents.event);
 
-router.get ("/api/getEvent",getEventsControllers.getEvents)
+router.get("/api/getEvent", getEventsControllers.getEvents);
+
 module.exports = router;
