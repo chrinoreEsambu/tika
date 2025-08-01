f8b98a4ae4560255ea655f9d91b25340835783c7
chrinore <chrinoren@gmail.com>
2025-08-01 20:59:38 +0100

    started

---
+++ backend/controllers/buyTicket.js
@@ -0,0 +1,12 @@
+const argon2 = require("argon2");
+const prisma = require("../config/prismaClient");
+const session = require("express-session");
+
+
+expots.buyTicket = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/controllers/getEvent.js
+++ backend/controllers/getEvent.js
@@ -18,7 +18,9 @@ exports.getEvents = async (req, res) => {
     });
     res.status(200).json({ message: "event available", findAllEvent });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res
+      .status(500)
+      .json({ message: "somethings went wrong", error: error.message });
   }
 };
 
@@ -44,6 +46,8 @@ exports.getEventsById = async (req, res) => {
     }
     res.status(200).json({ message: "event available", findAllEvent });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res
+      .status(500)
+      .json({ message: "somethings went wrong", error: error.message });
   }
 };
